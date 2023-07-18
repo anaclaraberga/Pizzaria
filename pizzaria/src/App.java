f27933337f097624bbbf61aa98e843ffe7308d3c
anaclaraberga <ana4ew.br@gmail.com>
2023-07-18 17:29:43 -0300

    ana

--- pizzaria/src/App.java
+++ pizzaria/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Desenvolvimento desenvolvimento = new Desenvolvimento();
-        desenvolvimento.desenvolvimento();
+        desenvolvimeno.desenvolvimento();
     }
 }
