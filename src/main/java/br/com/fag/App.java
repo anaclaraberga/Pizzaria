108af228f7e82c09b5e80f3d337bfc1fdcc538d5
Ana Clara <ana4ew.br@gmail.com>
2023-08-08 15:10:20 -0300

    Imprimir lista

--- src/main/java/br/com/fag/App.java
+++ src/main/java/br/com/fag/App.java
@@ -3,6 +3,7 @@ package br.com.fag;
 public class App {
     public static void main(String[] args) throws Exception {
         Desenvolvimento desenvolvimento = new Desenvolvimento();
+
         desenvolvimento.desenvolvimento();
     }
 }
--- src/main/java/br/com/fag/PizzaSabor.java
+++ src/main/java/br/com/fag/PizzaSabor.java
@@ -1,24 +1,50 @@
 package br.com.fag;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum PizzaSabor {
-  CALABRESA(10),
-  ROMANESCA(10),
-  PORTUGUESA(10),
-  FRANGO_COM_CATUPIRY(10),
-  LOMBO_COM_CATUPIRY(10),
-  CHOCOLATE_PRETO(12),
-  CHOCOLATE_BRANCO(12),
-  SENSACAO(12),
-  SEDUCAO(12),
-  BANOFFE(12);
+  CALABRESA("Calabresa", 10),
+  ROMANESCA("Romanesca", 10),
+  PORTUGUESA("Portuguesa",10),
+  FRANGO_COM_CATUPIRY("Frango com catupiry",10),
+  LOMBO_COM_CATUPIRY("Lombo com catupiry",10),
+  CHOCOLATE_PRETO("Chocolate preto",12),
+  CHOCOLATE_BRANCO("Chocolate branco",12),
+  SENSACAO("Sensação",12),
+  SEDUCAO("Sedução",12),
+  BANOFFE("Banoffe",12);
+
+  PizzaSabor(String name, float preco) {
+    precoSabor = preco;
+    nomeSabor = name;
+  }
 
   private final float precoSabor;
-
-  PizzaSabor(float preco) {
-    precoSabor = preco;
-  }
+  private final String nomeSabor;
 
   public float getPreco() {
     return precoSabor;
+  }
+
+  public String getNome() {
+    return nomeSabor;
+  }
+
+  public static String[] printAll() {
+    PizzaSabor[] sabores = PizzaSabor.values();
+    String[] arr = new String[sabores.length];
+    int i;
+    for(i = 0; i < sabores.length; i++) {
+      arr[i] = sabores[i].getNome();
+    }
+
+    return arr;
+  }
+
+  public static String getAll() {
+    return Stream.of(PizzaSabor.values()).
+                map(PizzaSabor::getNome).
+                collect(Collectors.joining("\n - "));
   }
 }
