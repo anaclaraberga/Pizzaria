5eec1180599c2a81a3ca48d6c3bb4c43809d60f9
Ana Clara Bergamini Delgado <ana4ew.br@gmail.com>
2024-01-24 18:13:35 -0300

    Getters e Setters

--- src/main/java/br/com/fag/TipoPagamento.java
+++ src/main/java/br/com/fag/TipoPagamento.java
@@ -1,5 +1,32 @@
 package br.com.fag;
 
 public class TipoPagamento {
-    
+    String credito;
+    String debito;
+    String pix;
+    String avista;
+
+    TipoPagamento(String credito, String debito, String pix, String avista) {
+        credito = credito;
+        debito = debito;
+        pix = pix;
+        avista = avista;
+    }
+
+    public String getCredito() {
+        return credito;
+    }
+
+    public String getDebito() {
+        return credito;
+    }
+
+    public String getPix() {
+        return credito;
+    }
+
+    public String getAvista() {
+        return credito;
+    }
+
 }
